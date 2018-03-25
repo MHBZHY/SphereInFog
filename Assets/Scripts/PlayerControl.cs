137e102acc75825676cd7e98cb3975445b1dde6a
mhbzhy <mianhuabao@126.com>
2018-03-25 10:21:19 +0800

    init

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 vec = new Vector3 (horizontal, 0.0f, vertical);
+		rb.AddForce (vec * speed);
+	}
+}
